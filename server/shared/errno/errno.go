3c6a594450f5dd4368e2bbaf7279f2a142b1506e
Galaxy <13087482+GalaxyPw@user.noreply.gitee.com>
2024-11-29 19:07:56 +0800

    Add config.yaml

---
+++ server/shared/errno/errno.go
@@ -0,0 +1,43 @@
+package errno
+
+import (
+	"fmt"
+	"github.com/rxdw-mall/server/shared/kitex_gen/errno"
+)
+
+type Errno struct {
+	ErrCode int64
+	ErrMsg  string
+}
+
+type Response struct {
+	Code    int64       `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+func (e Errno) Error() string {
+	return fmt.Sprintf("err_code:%d,err_msg:%d", e.ErrCode, e.ErrMsg)
+}
+
+func NewErrNo(code int64, msg string) Errno {
+	return Errno{
+		ErrCode: code,
+		ErrMsg:  msg,
+	}
+}
+
+func (e Errno) WithMessage(msg string) Errno {
+	e.ErrMsg = msg
+	return e
+}
+
+var (
+	Success        = NewErrNo(int64(errno.Err_Success), "success")
+	NoRoute        = NewErrNo(int64(errno.Err_NoRoute), "noRoute")
+	NoMethod       = NewErrNo(int64(errno.Err_NoMethod), "NoMethod")
+	BadRequest     = NewErrNo(int64(errno.Err_BadRequest), "BadRequest")
+	ParamsErr      = NewErrNo(int64(errno.Err_ParamsErr), "paramsErr")
+	AuthorizeFail  = NewErrNo(int64(errno.Err_AuthorizeFail), "AuthorizeFail")
+	TooManyRequest = NewErrNo(int64(errno.Err_TooManyRequest), "Too many requests")
+)
